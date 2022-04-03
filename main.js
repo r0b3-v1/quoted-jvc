6e963f5afbca7ff7d9030d5c54a202ae1a3ea413
RBAGES <bagesremy@gmail.com>
2022-04-03 15:38:31 +0200

    version

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Quoted
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.6.2
 // @description  affiche qui vous cite dans le topic et vous permet d'accéder au message directement en cliquant sur le lien, même s'il est sur un page différente!
 // @author       Dereliction
 // @match        https://www.jeuxvideo.com/forums/*
