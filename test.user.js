9538508096a241d721e7f72f243a9d1e42460164
RBAGES <bagesremy@gmail.com>
2022-04-08 13:58:26 +0200

    test

--- test.user.js
+++ test.user.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.1
 // @description  test autoupdate
-// @downloadURL  https://github.com/r0b3-v1/quoted-jvc/raw/main/main.user.js
-// @updateURL    https://github.com/r0b3-v1/quoted-jvc/raw/main/main.user.js
+// @downloadURL  https://github.com/r0b3-v1/quoted-jvc/raw/main/test.user.js
+// @updateURL    https://github.com/r0b3-v1/quoted-jvc/raw/main/test.user.js
 // @author       Dereliction
 // @match        https://www.jeuxvideo.com/forums/*
 // @license      Exclusive Copyright
