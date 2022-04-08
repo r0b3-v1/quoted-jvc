9739fbf0946d8ab5282e5ecccecdfc893d812e9f
RBAGES <bagesremy@gmail.com>
2022-04-08 13:51:45 +0200

    rename files

---
+++ test.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         testatest
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  test autoupdate
+// @author       Dereliction
+// @match        https://www.jeuxvideo.com/forums/*
+// @license      Exclusive Copyright
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// ==/UserScript==
+
+
+
+'use strict';
+
+let i = 'test';
